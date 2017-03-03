88c3adc42e1d815403939fb0a67c9cd06f5b2b88
Gaurav Gada <gauravgada@gmail.com>
2017-03-03 02:35:28 -0800

    Site updates
    Added
    - Play/Pause controls
    - Inflation
    - Trade
    - Year next/prev

---
+++ site/js/main.js
@@ -0,0 +1,130 @@
+// TODO
+// Button highlight logic
+
+let map;
+let initYear;
+let stopYear;
+let x;
+let yearDiv = document.querySelector('#year');
+
+let config = {
+    unemployment: {
+        file: 'data/unemployment.csv',
+        colorscheme: colorbrewer.YlOrRd[9],
+        startYear: 1991,
+        stopYear: 2014
+    },
+    trade: {
+        file: 'data/trade.csv',
+        colorscheme: colorbrewer.YlGn[9],
+        startYear: 1960,
+        stopYear: 2015
+    },
+    inflation: {
+        file: 'data/inflation.csv',
+        colorscheme: colorbrewer.YlOrRd[9],
+        startYear: 1960,
+        stopYear: 2015
+    }
+}
+
+let format = function(d) {
+    return d3.format(',.02f')(d) + '%';
+}
+
+function initMap(year, colorscheme) {
+    initYear = year;
+    x = initYear;
+    map = d3.geomap.choropleth()
+        .geofile('data/countries.json')
+        .colors(colorscheme)
+        .column(year)
+        .format(format)
+        .legend(true)
+        .unitId('Country Code');
+}
+
+function dataBind(filename) {
+    d3.csv(filename, function(error, data) {
+        d3.select('#map')
+            .datum(data)
+            .call(map.draw, map);
+    });
+}
+
+function iterMap(stopYear, direction) {
+    if (direction == 'next') {
+        if (x >= stopYear) {
+            x = initYear;
+            updateYear(x);
+            return;
+        }
+        x++;
+        updateYear(x);
+    }
+    if (direction == 'prev') {
+        if (x <= initYear) {
+            x = stopYear;
+            updateYear(x);
+            return;
+        }
+        x--;
+        updateYear(x);
+    }
+}
+
+function updateYear(year) {
+    yearDiv.innerHTML = year;
+    map.properties.column = year;
+    map.update();
+}
+
+let mapInterval = null;
+
+function buttonHandler(e) {
+
+    // console.log(e.target.classList.contains('next'));
+    // console.log(e.target.dataset.action);
+
+    if (e.target.dataset.action == 'play-pause') {
+        if (typeof(map) == 'undefined') {
+            yearDiv.innerHTML = "First select a feature to initialize the map!";
+            return;
+        }
+        if (mapInterval === null) {
+            console.log(e.target.dataset.action);
+            let callFunc = 'iterMap(' + stopYear + ', "next")';
+            console.log(callFunc);
+            mapInterval = setInterval(callFunc, 400);
+            // mapInterval = setInterval('iterMap(2014, "next")', 400);
+        } else {
+            window.clearInterval(mapInterval);
+            mapInterval = null;
+        }
+        return;
+    }
+
+    if (e.target.dataset.action == 'next' || e.target.dataset.action == 'prev') {
+        if (typeof(map) == 'undefined') {
+            yearDiv.innerHTML = "First select a feature to initialize the map!";
+            return;
+        }
+        iterMap(stopYear, e.target.dataset.action)
+        return;
+    }
+
+    d3.select('svg').remove();
+
+    dataBind(config[`${e.target.dataset.action}`].file);
+    initMap(config[`${e.target.dataset.action}`].startYear, config[`${e.target.dataset.action}`].colorscheme);
+    stopYear = config[`${e.target.dataset.action}`].stopYear;
+    window.clearInterval(mapInterval);
+    let callFunc = 'iterMap(' + stopYear + ', "next")';
+    mapInterval = setInterval(callFunc, 400);
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(b => {
+    b.addEventListener('click', buttonHandler)
+});
